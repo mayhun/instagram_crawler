2152e5d7bbe4e972d83a5352c94cfe084a57b9b1
mayhun <mayhun28@gmail.com>
2025-07-21 13:28:11 +0900

    feat(redis_client): redis 접속 설정

---
+++ app/redis_client.py
@@ -0,0 +1,12 @@
+import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+redis_client = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    password=os.getenv("REDIS_PASSWORD", None),
+    decode_responses=True
+)
